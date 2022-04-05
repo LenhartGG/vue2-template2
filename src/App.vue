1cbebbff67add0916b0fc3fee027b60232a683c5
lenhartgg <lenhartgg@163.com>
2022-04-05 21:01:26 +0800

    解决刷新页面vuex数据丢失问题

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,29 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/progress">ProgressBar</router-link> |
       <router-link to="/treelist">treelist</router-link> |
+      <router-link to="/test">test</router-link> |
     </nav>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  created () { //页面刷新store数据备份
+    // if (localStorage.cmstoken) {
+    // this.$router.replace("/data");
+    // }else {
+    // this.$router.replace("/");
+    // }
+    if (sessionStorage.getItem("store") ) { //在页面加载时读取sessionStorage里的状态信息
+      this.$store.replaceState(Object.assign({}, this.$store.state,JSON.parse(sessionStorage.getItem("store"))))
+    }
+    window.addEventListener("beforeunload",()=>{ //在页面刷新时将vuex里的信息保存到sessionStorage里
+      sessionStorage.setItem("store",JSON.stringify(this.$store.state))
+    })
+  },
+}
+</script>
 
 <style lang="scss">
 #app {
