2f70e86776e2219f2a25262699b2b4e932fcd067
lenhartgg <lenhartgg@163.com>
2022-04-08 18:07:39 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
+  outputDir: 'dist',
+  assetsDir: '',
+  indexPath: 'index.html',
+  filenameHashing: true,
+  lintOnSave: process.env.NODE_ENV !== 'production',  //保存时 lint 代码
+  transpileDependencies: false, //转译第三方依赖
+  productionSourceMap: false, //生产环境的 source map
+  integrity: false, //子资源完整性
+
 })
